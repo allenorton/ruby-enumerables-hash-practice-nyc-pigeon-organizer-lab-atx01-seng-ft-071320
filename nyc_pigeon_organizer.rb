f017c45be228b171684dda3528d3aadd43671d42
Allene Norton <norton.allene@gmail.com>
2020-06-29 20:23:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), finA|
-    finA[key]=value
-    binding.pry 
+    value.each do |in_key, names|
+      binding.pry
+    end 
+    #binding.pry 
   end 
 end
